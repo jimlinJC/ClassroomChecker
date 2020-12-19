51b3f999527e9fbbe25282f8b51b91763379b8f0
jimlinJC <47108352+jimlinJC@users.noreply.github.com>
2020-12-19 21:01:49 +0800

    Update views.py

--- show/views.py
+++ show/views.py
@@ -7,40 +7,40 @@ today = datetime.date.today()
 now = datetime.datetime.now()
 nextClass = now+datetime.timedelta(0,hours=1)
 src = "http://stuinfo.ntust.edu.tw/classroom_user/classroom_usecondition.aspx"
-period = []
-period.append([])
-period.append([])
-period[0].append('0')
-period[0].append('0800')
-period[0].append('0901')
-period[0].append('1001')
-period[0].append('1111')
-period[0].append('1211')
-period[0].append('1311')
-period[0].append('1411')
-period[0].append('1511')
-period[0].append('1621')
-period[0].append('1721')
-period[0].append('1821')
-period[0].append('1916')
-period[0].append('2011')
-period[0].append('2106')
+timePeriod = []
+timePeriod.append([])
+timePeriod.append([])
+timePeriod[0].append('0')
+timePeriod[0].append('0800')
+timePeriod[0].append('0901')
+timePeriod[0].append('1001')
+timePeriod[0].append('1111')
+timePeriod[0].append('1211')
+timePeriod[0].append('1311')
+timePeriod[0].append('1411')
+timePeriod[0].append('1511')
+timePeriod[0].append('1621')
+timePeriod[0].append('1721')
+timePeriod[0].append('1821')
+timePeriod[0].append('1916')
+timePeriod[0].append('2011')
+timePeriod[0].append('2106')
 # -----------------------------------
-period[1].append('0')
-period[1].append('0900')
-period[1].append('1000')		
-period[1].append('1110')
-period[1].append('1210')
-period[1].append('1310')
-period[1].append('1410')
-period[1].append('1510')
-period[1].append('1620')
-period[1].append('1720')
-period[1].append('1820')
-period[1].append('1915')
-period[1].append('2010')
-period[1].append('2105')
-period[1].append('2200')
+timePeriod[1].append('0')
+timePeriod[1].append('0900')
+timePeriod[1].append('1000')		
+timePeriod[1].append('1110')
+timePeriod[1].append('1210')
+timePeriod[1].append('1310')
+timePeriod[1].append('1410')
+timePeriod[1].append('1510')
+timePeriod[1].append('1620')
+timePeriod[1].append('1720')
+timePeriod[1].append('1820')
+timePeriod[1].append('1915')
+timePeriod[1].append('2010')
+timePeriod[1].append('2105')
+timePeriod[1].append('2200')
 # ------------------------------------
 
 
@@ -58,8 +58,8 @@ def checkTRclassroomNow():
     tabledata = getClassroom()
     for classroom in tabledata:
         for i in range(1,15):
-            # print(int(period[0][i]),'--',int(now.strftime('%H%M')),'--',int(period[1][i]),'--',int(now.strftime('%H%M')))
-            if(int(period[0][i]) <= int(now.strftime('%H%M')) and int(period[1][i]) >= int(now.strftime('%H%M'))): 
+            # print(int(timePeriod[0][i]),'--',int(now.strftime('%H%M')),'--',int(timePeriod[1][i]),'--',int(now.strftime('%H%M')))
+            if(int(timePeriod[0][i]) <= int(now.strftime('%H%M')) and int(timePeriod[1][i]) >= int(now.strftime('%H%M'))): 
                 if(tabledata[classroom][i] == ''):
                     print(classroom)
                     theNoneUsed.append(classroom)
@@ -78,7 +78,7 @@ def checkTRclassroomNext():
     tabledata = getClassroom()
     for classroom in tabledata:
         for i in range(1,15):
-            if(int(period[0][i]) <= int(nextClass.strftime('%H%M')) and int(period[1][i]) >= int(nextClass.strftime('%H%M'))): 
+            if(int(timePeriod[0][i]) <= int(nextClass.strftime('%H%M')) and int(timePeriod[1][i]) >= int(nextClass.strftime('%H%M'))): 
                 if(tabledata[classroom][i] == ''):
                     print(classroom)
                     theNoneUsed.append(classroom)
@@ -125,7 +125,7 @@ def getClassroom():
 
     
     #             
-    #         elif(int(period[0][i]) > int(nextClass.strftime('%H%M')) and int(period[1][i-1]) < int(nextClass.strftime('%H%M'))):
+    #         elif(int(timePeriod[0][i]) > int(nextClass.strftime('%H%M')) and int(timePeriod[1][i-1]) < int(nextClass.strftime('%H%M'))):
     #             nowIsBreakTime = True
 
 
